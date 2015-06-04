ab0640d7b0197270fa76e86151f6f6974c7534dc
ldf7801528 <690115491@qq.com>
2015-06-04 17:14:30 +0800

    verson 1.0 -- app.js edit

--- app/app.js
+++ app/app.js
@@ -1,3 +1,13 @@
 /**
  * Created by yaxin on 2015/6/4.
  */
+Ext.regApplication({
+    /*定义应用程序名称，并作为命名空间*/
+    name: 'app',
+    /*默认目标名称*/
+    defaultTarget: 'viewport',
+    /*初始化函数*/
+    launch: function(){
+        this.viewport = new app.views.viewport();
+    }
+});
--- app/view/view.js
+++ app/view/view.js
@@ -1,3 +1,21 @@
 /**
  * Created by yaxin on 2015/6/4.
  */
+app.views.viewport = Ext.extend(Ext.Panel,{
+    id: 'viewport',
+    fullscreen: true,
+    layout: 'card',
+    activeItem: 0,
+    items: [
+        {
+            xtype:'listPanel'
+        },{
+            xtype:'noteForm'
+        },{
+            xtype:'viewNote'
+        }
+    ],
+    initComponent: function(){
+        app.views.viewport.superclass.initComponent.apply(this,arguments);
+    }
+});
